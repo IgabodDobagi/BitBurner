c521e3fdbed6b0ee3f42e99946856050865ad252
IgabodDobagi <114884384+IgabodDobagi@users.noreply.github.com>
2022-10-02 18:05:10 -0400

    Create colortest.js

---
+++ colortest.js
@@ -0,0 +1,20 @@
+// Syntax is simple. Just run colortest.js
+// Code is simple but was taken from the Bitburner Discord and provided by Thatman
+/** @param {NS} ns */
+export async function main(ns) {
+
+    const col = {
+        "r": "\x1b[31m",
+        "g": "\x1b[32m",
+        "b": "\x1b[34m",
+        "c": "\x1b[36m",
+        "m": "\x1b[35m",
+        "y": "\x1b[33m",
+        "bk": "\x1b[30m",
+        "w": "\x1b[37m",
+        "d": "\x1b[0m" //default color
+    }
+    ns.tprint(col.bk + "black " + col.r + "red " + col.g + "green " + col.y + "yellow "
+        + col.b + "blue " + col.m + "magenta " + col.c + "cyan " + col.w + "white " + col.d + "default")
+
+}
